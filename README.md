2fe21f855748d305a18dc015e4f7f49d01abbf5f
johney <johneyburke@gmail.com>
2023-11-21 14:21:54 -0700

    final version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # SENG-FLEX-082823-Project-1
 First Project - show ranker with local json DB
+
+## Learning Goals
+
+- Design and architect features across a frontend
+- Communicate and collaborate in a technical environment
+- Integrate JavaScript and an external API
+- Debug issues in small- to medium-sized projects
+- Build and iterate on a project MVP
+
+## Overveiw
+
+This project is a simple show ranker site. 
+Users can see a list of 5 popular shows and highlight the ones that they like best in this list
+
+## Details
+
+JSON DB - This site relies on a JSON db that serves as a controlled data source. 
+The original content came from https://api.tvmaze.com/singlesearch/shows 
+This data was cleaned and abbeviated to a simpler data set.
+
+JS - The js fetches the data from the db and renders it in order by ID
+Inner HTML is used to create the show cards 
+The site listens to the dom and uses create element to build the HTML, 
+query selector to select individual element IDs and event listener to color the users favorite show
+JS functions are used to fetch the db data and create the show cards 
+
+HTML - The HTML is primarily a shell for the JS to add elements to
+The form is the primary site element that is built in HTML to intake a users email
+
+CSS - CSS is also a shell that provides very simple styles for the site elements created by the js
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,63 @@
+
+
+
+
+  //DOM Render functions
+  function renderShow(show){
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML =`
+    <img src="${show.image}">
+    <div class="content"> 
+    <h4> ${show.name} </h4>
+    <p>
+        ${show.network}
+    </p>
+    <p>
+        ${show.summary}
+    </p>
+    </div>
+    <div class="buttons">
+    <button id="${show.id}"> Upvote </button> 
+    </div>
+    `
+    document.querySelector('#show-list').appendChild(card);
+    const upvoteButton = document.getElementById(`${show.id}`);
+    upvoteButton.addEventListener('click', colorElement);
+    
+  }
+
+  // fetch data from api
+  function getAllShows(){
+    fetch('http://localhost:3000/showData')
+    .then (res => res.json())
+    .then (showData => showData.forEach(show =>renderShow(show))) ;
+  }
+  
+ 
+  // initial render
+  //get data and render shows to dom
+
+  function intialize(){
+    getAllShows()
+    
+  }
+
+  intialize();
+  
+
+ 
+
+  function colorElement(event) {
+  const element = event.target;
+  element.style.color = 'yellow';
+}
+
+  const emailForm = document.getElementById('#form');
+  emailForm.addEventListener('submit', formSubmit);
+      
+  function formSubmit(event) {
+    event.preventDefault(); // Prevent default form submission
+    const form = event.target; // Get the form element that triggered the event
+ 
+  }
